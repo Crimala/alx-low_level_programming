54877729f8c09ac67813251b82e254a5bfe36894
Crimala <sandyangel235@gmail.com>
2023-02-16 17:48:38 -0800

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,13 +5,13 @@
  * main prints "Programming is like building a multilingual puzzle"
  * followed by a new line
  * return 0.
- * */
+ */
 
 
   int main()
 
 {
-	puts("\"Programmining is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 }
