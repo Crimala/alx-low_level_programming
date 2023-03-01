b4729a98f231dabfce91c56db8bbc2d38ddf2930
Crimala <sandyangel235@gmail.com>
2023-03-01 12:03:39 -0800

    Corrtask8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * main - prints exactly and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19,
  * followed by a new line, to the standard error
  * Return: 1 (Always)
  */
 
 int main(void)
 {
-	write(2, "and that piece of rt is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
